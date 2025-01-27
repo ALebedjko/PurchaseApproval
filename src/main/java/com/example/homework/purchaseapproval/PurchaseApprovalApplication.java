53757d43354d7b76384e0f263f3bd46e6796955b
ALebedjko <enfocer@gmail.com>
2025-01-27 16:46:26 +0200

    Initialize Spring Boot project with PostgreSQL and Gradle setup
    Added a Spring Boot project for the "PurchaseApproval" application using Gradle as the build tool. Configured PostgreSQL as the database including `application.properties` and Docker Compose for the environment setup. Included boilerplate project structure, Gradle wrapper, and initial test cases.

---
+++ src/main/java/com/example/homework/purchaseapproval/PurchaseApprovalApplication.java
@@ -0,0 +1,13 @@
+package com.example.homework.purchaseapproval;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PurchaseApprovalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PurchaseApprovalApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/homework/purchaseapproval/PurchaseApprovalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.homework.purchaseapproval;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PurchaseApprovalApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
